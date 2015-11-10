da15bc9d3a75350e2b486f18ae8ebbc97fb27b27
Gabriel <mutatisguga@gmail.com>
2015-11-10 17:02:58 -0200

    f

---
+++ ElderChef/Assets/Script/Alimento/Alimento.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Alimento : MonoBehaviour
+{
+
+    public Rigidbody2D rig;
+
+    int minX;
+    int maxX;
+    int bloco;
+
+	void Start ()
+    {
+        Jogar(-5, 0);
+	}
+	
+	void Update ()
+    {
+        bloco = PlayerMovment.player.bloco;
+	}
+
+    public void Calcula()
+    {
+        if (bloco != 1)
+        {
+            minX = bloco * -1;
+        }
+        else
+        {
+            minX = 0;
+        }
+        maxX = ((bloco - 5) * -1);
+        print(minX + "+" + maxX + "+" + bloco);
+    }
+
+    public void Jogar(int min, int max)
+    {
+        int x = Random.Range(min, max);
+        int y = 10;
+        rig.velocity = new Vector3(x, y, 0);
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Calcula();
+            Jogar(minX, maxX);
+        }
+    }
+}
---
+++ ElderChef/Assets/Script/Player/PlayerMovment.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovment : MonoBehaviour
+{
+    public static PlayerMovment player;
+    
+    public float velX;
+
+    [HideInInspector]
+    public int bloco = 3;
+	
+    void Awake()
+    {
+        player = this;
+    }
+
+	void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftArrow) && bloco > 1)
+        {
+            if(transform.localScale.x > 0)
+            {
+                transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+            }
+            else
+            {
+                bloco -= 1;
+                transform.Translate((velX * -1), 0, 0);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow) && bloco < 5)
+        {
+            if (transform.localScale.x < 0)
+            {
+                transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+            }
+            else
+            {
+                bloco += 1;
+                transform.Translate(velX, 0, 0);
+            }
+        }
+    }
+}
