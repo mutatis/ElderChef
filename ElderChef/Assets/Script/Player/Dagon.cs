0958b2de69228e4cee3f0d2799409c01264eea30
Gabriel <mutatisguga@gmail.com>
2015-11-24 16:08:13 -0200

    f

--- ElderChef/Assets/Script/Player/Dagon.cs
+++ ElderChef/Assets/Script/Player/Dagon.cs
@@ -6,23 +6,11 @@ public class Dagon : MonoBehaviour
 
     public Animator anim;
 
-    GameObject obj;
-
-    public void Comeu()
-    {
-        if (obj.GetComponent<Alimento>().cozinho <= 0 && obj.GetComponent<Alimento>().frito > 0)
-        {
-            LevelManager.levelManager.comeu += 1;
-            Destroy(obj);
-        }
-    }
-
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Alimento")
         {
             anim.SetTrigger("Comeu");
-            obj = other.gameObject;
         }
     }
 }
---
+++ ElderChef/Assets/Script/Player/DagonBoca.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DagonBoca : MonoBehaviour
+{
+    public Animator dagon;
+
+    GameObject obj;
+
+    IEnumerator GO()
+    {
+        yield return new WaitForSeconds(0.15f);
+        LevelManager.levelManager.comeu += 1;
+        Destroy(obj);
+        StopCoroutine("GO");
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Alimento")
+        {
+            obj = other.gameObject;
+          //  if (obj.GetComponent<Alimento>().cozinho <= 0 && obj.GetComponent<Alimento>().frito > 0)
+         //   {
+                dagon.SetTrigger("Mastiga");
+                StartCoroutine("GO");
+           // }
+        }
+    }
+}
