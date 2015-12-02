c1bbb43af9ddf488f63d29c977ed12a505b39be9
Gabriel <mutatisguga@gmail.com>
2015-12-02 15:45:34 -0200

    New Assets

---
+++ ElderChef/Assets/Script/Interface/Achievement/AchievementInterface.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AchievementInterface : MonoBehaviour
+{
+    public Text text;
+
+    public string achiName;
+
+    void Start()
+    {
+        text.text = achiName;
+    }
+}
---
+++ ElderChef/Assets/Script/Interface/Achievement/AchievementUnlock.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AchievementUnlock : MonoBehaviour
+{
+
+    public GameObject achievement;
+
+	void Update ()
+    {
+	    
+	}
+}
