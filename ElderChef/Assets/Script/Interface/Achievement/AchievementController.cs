05e43f88bae05a6251f8de10aa986cfa14555d07
Gabriel <mutatisguga@gmail.com>
2015-12-03 11:22:52 -0200

    Achievement

---
+++ ElderChef/Assets/Script/Interface/Achievement/AchievementController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AchievementController : MonoBehaviour
+{
+    public GameObject obj;
+
+    public void DestroyObj()
+    {
+        Destroy(obj);
+    }
+}
--- ElderChef/Assets/Script/Interface/Achievement/AchievementUnlock.cs
+++ ElderChef/Assets/Script/Interface/Achievement/AchievementUnlock.cs
@@ -8,6 +8,15 @@ public class AchievementUnlock : MonoBehaviour
 
 	void Update ()
     {
-	    
+	    if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Unlock("GGGGGG");
+        }
 	}
+
+    void Unlock(string name)
+    {
+        GameObject obj = Instantiate(achievement) as GameObject;
+        obj.gameObject.GetComponent<AchievementInterface>().achiName = name;
+    }
 }
