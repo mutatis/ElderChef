85daaf4185544f252706d5b98ee0da44a77a3f92
Gabriel <mutatisguga@gmail.com>
2015-12-09 14:24:49 -0200

    bug fixes

---
+++ ElderChef/Assets/Script/Interface/Start/TapStart.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TapStart : MonoBehaviour
+{
+    void Start()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void Click(GameObject obj)
+    {
+        print("OOOOII");
+        Time.timeScale = 1;
+        Destroy(obj);
+    }
+}
